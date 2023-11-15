6e68a3c04c6decdaacbd4906eca6cdc9ebef0a9d
OleJG <flucode9@gmail.com>
2023-11-14 20:57:47 -0500

    test code

---
+++ company_management/index.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'yourUsername',
+    password: 'yourPassword',
+    database: 'company_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    startApp();
+});
+
+function startApp() {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    })
+    .then(answer => {
+        switch (answer.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            // Add cases for each option
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    });
+}
+
+function viewDepartments() {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
